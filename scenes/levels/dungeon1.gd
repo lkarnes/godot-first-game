d9b657f1fdfda9cab0e37fa0ee2924f51bb308d3
Logan Karnes <logan.karnes98@gmail.com>
2024-04-21 00:03:58 -0700

    path gen functional, needs algo to complete, also should include a dynamic path witdth

--- scenes/levels/dungeon1.gd
+++ scenes/levels/dungeon1.gd
@@ -14,46 +14,48 @@ func create_backdrop():
 		for x in wid:
 			coords_array.append(Vector2i(x - (x/2),y - (y/2)));
 	tileMap.set_cells_terrain_connect(0, coords_array, 0, 1);
-	var path = build_path();
-	tileMap.set_cells_terrain_connect(0, path, 0, 0);
+	build_path();
 	
 	
 func build_path():
 	var path_arr: Array[Vector2i] = [Vector2i(10, hght/2)]
 	var current = Vector2i(15, 15);
-	var moves = 5;
+	var moves = 100;
 	
 	while moves > 0:
 		moves -= 1;
-		var possible_moves: Array = []
-		var random_length = randf_range(1,10);
+		var possible_moves: Array = [
+			Vector2i(current.x + 1, current.y),
+			Vector2i(current.x - 1, current.y),
+			Vector2i(current.x, current.y + 1),
+			Vector2i(current.x, current.y - 1)
+		];
+		var direction_arr = [];
+		var random_length = randf_range(4,10);
 		if current.x + random_length < wid:
-			possible_moves.append(Vector2i(current.x + 1, current.y));
+			direction_arr.append('right');
 		if current.x - random_length > 0:
-			possible_moves.append(Vector2i(current.x - 1, current.y));
+			direction_arr.append('left');
 		if current.y + random_length < hght:
-			possible_moves.append(Vector2i(current.x, current.y + 1))
+			direction_arr.append('down');
 		if current.y - random_length > 0:
-			possible_moves.append(Vector2i(current.x, current.y - 1))
-		current = possible_moves.pick_random()
-		add_line(path_arr, random_length, current);
-	return path_arr;
+			direction_arr.append('up');
+		var rand_direction = direction_arr.pick_random()
+		match rand_direction:
+			'right':
+				current = possible_moves[0]
+			'left':
+				current = possible_moves[1]
+			'down':
+				current = possible_moves[2]
+			'up':
+				current = possible_moves[3]
+		add_line(path_arr, random_length, current, rand_direction);
+	tileMap.set_cells_terrain_connect(0, path_arr, 0, 0);
+
 		
-func add_line(path_arr: Array, rng, current):
-	
+func add_line(path_arr: Array, rng, current, dir):
 	var prev = path_arr[path_arr.size() - 1]
-	var dif = Vector2(prev.x - current.x, prev.y - current.y);
-	var dir: String;
-	print('dif', dif);
-	#find path direction
-	if dif.x < 0:
-		dir = 'left';
-	elif dif.x > 0:
-		dir = 'right';
-	elif dif.y < 0:
-		dir = 'up';
-	else:
-		dir = 'down';
 	match dir:
 		'up':
 			for i in range(rng):
