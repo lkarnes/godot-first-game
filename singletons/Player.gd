6274bda92ef485984eeff0164996ee2a762add23
Logan Karnes <logan.karnes98@gmail.com>
2024-05-02 20:03:46 -0700

    added infinite chunk loader

--- singletons/Player.gd
+++ singletons/Player.gd
@@ -4,7 +4,7 @@ extends Node
 var player: CharacterBody2D;
 var health: float = 100;
 var mana: float = 100;
-var stamina: float = 100;
+var position: Vector2 = Vector2.ZERO
 
 func set_position(pos: Vector2 = Vector2.ZERO):
 	if player:
